42d37f7850476de6d6d84084cc3b56ef305b7922
Asrar Ahammad <asrarahammadshaik@gmail.com>
2024-06-16 10:47:40 +0530

    Completed Project setup

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className="nav-container">
+            <a href="/">InkVerse</a>
+            <div className="nav-menu">
+                <a href="/">Home</a>
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
